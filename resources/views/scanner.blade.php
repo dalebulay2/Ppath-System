c20d99165d6af05c6cf03c44978c75b77f075169
dalebulay8 <dalebulay8@gmail.com>
2026-06-25 14:58:47 +0800

    Create scanner.blade.php

---
+++ resources/views/scanner.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PPATH QR Scanner</title>
+</head>
+<body>
+
+    <h1>PPATH QR Scanner</h1>
+
+    <p>Scanner page created successfully.</p>
+
+</body>
+</html>
